5fa2f6d671438ae4c41b835739366030fd2a7fe5
Gerald Silverio <gasilverio@banreservas.com>
2026-05-07 16:38:42 -0400

    Rename sourcer folder

---
+++ src/Rentify.Backend.Core.Application/Helpers/ReadFromConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rentify.Backend.Core.Application.Helpers
+{
+    public static class ReadFromConfiguration
+    {
+        /// <summary>
+        /// Metodo que se encarga de retonar el valor obtenido desde una var de enviroment o desde el config del aplicativo
+        /// </summary>
+        /// <param name="config"></param>
+        /// <param name="keyConfig"></param>
+        /// <returns></returns>
+        public static string GetValueFromConfig(IConfiguration config, string keyConfig)
+        {
+            string? varConfig = config.GetValue<string>(keyConfig);
+            if (!string.IsNullOrEmpty(varConfig))
+            {
+                return varConfig;
+            }
+
+            throw new InvalidOperationException($"No se encontró configuración para '{keyConfig}' en el entorno.");
+        }
+    }
+}
---
+++ src/Rentify.Backend.Core.Application/ServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Rentify.Backend.Core.Application
+{
+    /// <summary>
+    /// Static class for registering application layer services.
+    /// </summary>
+    public static class ServiceRegistration
+    {
+        /// <summary>
+        /// Registers application layer services such as AutoMapper and MediatR.
+        /// </summary>
+        /// <param name="services">The collection of services to add to.</param>
+        public static void AddApplicationLayer(this IServiceCollection services)
+        {
+            
+        }
+    }
+}
