d53b7fce1a65741d96daa04a9a8511c2fb564388
Gerald Silverio <gasilverio@banreservas.com>
2026-06-13 01:31:13 -0400

    feat: send password reset emails

--- Rentify.Backend/Rentify.Backend.Core.Application/Modules/Secutiry/Commands/ForgotPassword/ForgotPasswordCommand.cs
+++ Rentify.Backend/Rentify.Backend.Core.Application/Modules/Secutiry/Commands/ForgotPassword/ForgotPasswordCommand.cs
@@ -3,5 +3,7 @@ using Rentify.Backend.Core.Application.Shared.Response;
 
 namespace Rentify.Backend.Core.Application.Modules.Secutiry.Commands.ForgotPassword
 {
-    public record ForgotPasswordCommand(string Email) : IRequest<ResultReponse<ForgotPasswordResponse>>;
+    public record ForgotPasswordCommand(
+        string Email,
+        string ResetPasswordUrl) : IRequest<ResultReponse<ForgotPasswordResponse>>;
 }
---
+++ Rentify.Backend/Rentify.Backend.Core.Application/Modules/Secutiry/EmailTemplateCodes.cs
@@ -0,0 +1,7 @@
+namespace Rentify.Backend.Core.Application.Modules.Secutiry
+{
+    public static class EmailTemplateCodes
+    {
+        public const string PasswordReset = "PASSWORD_RESET";
+    }
+}
